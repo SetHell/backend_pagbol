d0ea97425fe9bcaff59e7ec8c2cce6785b4e0a18
SetHMode <prueba>
2026-06-11 08:48:30 -0400

    fix typeorm connection config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -10,15 +10,9 @@ export const databaseConfig = (
   username: config.get<string>('DB_USER'),
   password: config.get<string>('DB_PASS'),
   database: config.get<string>('DB_NAME'),
-
   autoLoadEntities: true,
   synchronize: false,
-
   extra: {
-    connectionLimit: 1,
-    queueLimit: 0,
+    connectionLimit: 2,
   },
-
-  retryAttempts: 1,
-  retryDelay: 3000,
 });
