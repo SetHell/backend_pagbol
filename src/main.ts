9fb6779ee139408a477cc41f6ec99523c6504571
SetHMode <prueba>
2026-06-11 08:31:47 -0400

    fix backend clean nul issue

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const obtenerOrigenesPermitidos = () => {
   const origenes = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://frontend-pagbol.vercel.app/',
     process.env.FRONTEND_URL,
   ];
 
